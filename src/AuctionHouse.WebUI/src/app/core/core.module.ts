0eb8fed1034157646a1f75ea0f1a4cbce09d4661
Matthew Jones <mail@matthewjones.io>
2022-11-03 16:02:02 +0000

    Adds material library to WebUI and adds Login and Registration modules.

--- src/AuctionHouse.WebUI/src/app/core/core.module.ts
+++ src/AuctionHouse.WebUI/src/app/core/core.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -14,7 +22,17 @@ import { ErrorInterceptor } from './interceptors/error.interceptor';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    RouterModule,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
